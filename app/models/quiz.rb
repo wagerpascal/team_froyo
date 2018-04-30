20ebdcf0f12d569805468a25714722c2f791cdd4
Hang Li <hangli@tamu.edu>
2018-04-30 00:26:21 -0500

    clean

--- app/models/quiz.rb
+++
@@ -1,6 +0,0 @@
-class Quiz < ApplicationRecord
-  belongs_to :topic
-  validates :question, presence: true, length: { maximum: 500 }
-  validates :answer, presence: true, length: { maximum: 500 }
-  validates :remark, length: { maximum: 500 }
-end
